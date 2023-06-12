61743dc58a8cc72554da3ccbfb0dd9ec424ccb7f
Yogiraj H <yogiraj@nilenso.com>
2023-06-12 12:55:10 +0530

    chore: integrate nom parser

--- ace/src/parser.rs
+++ ace/src/parser.rs
@@ -1,15 +1,26 @@
-
-pub fn parse(input: String) -> String {
-    String::from("")
+use nom::bytes::complete::{tag, take_until};
+use nom::character::complete::space0;
+use nom::sequence::{delimited, tuple};
+use nom::IResult;
+pub fn parse(input: &str) -> IResult<&str, &str> {
+    delimited(tuple((tag("[["), space0)), take_until("]]"), tuple((space0, tag("]]"))))(input)
 }
 
 #[cfg(test)]
 mod tests {
+    use super::parse;
+
     #[test]
     fn it_parses_a_link() {
-        let test_input = "";
-        let r = 2 + 2;
+        let test_input: &str = "[[Gjgsdgfjhgasj]]";
+        let x = parse(test_input);
+        assert_eq!(x, Ok(("", "Gjgsdgfjhgasj")));
+    }
 
-        assert_eq!(r, 4);
+    #[test]
+    fn it_parses_a_link_with_surrounding_whitespace() {
+        let test_input: &str = "[[ Apple HJhksahf ]]";
+        let x = parse(test_input);
+        assert_eq!(x, Ok(("", " Apple HJhksahf ")));
     }
 }
